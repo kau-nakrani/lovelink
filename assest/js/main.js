33e692549578647335bf681a20bc993045e50196
kau-nakrani <knakrani100@gmail.com>
2024-06-13 14:26:38 +0530

    add stucutre

--- assest/js/main.js
+++ assest/js/main.js
@@ -1,8 +1,16 @@
-/******** JS INDEX *********/
+/******** JS INDEX *********
+  1.Multi step form
+    1.1 Phone number
+******** JS INDEX *********/
 
 (function ($) {
+
+  //1.1 Phone number
   $("#mobile_code").intlTelInput({
     initialCountry: "in",
     separateDialCode: true,
   });
+
+  
+
 })(jQuery);
